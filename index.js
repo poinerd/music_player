422ec93e13ca4f38a78297f0ec817651cc7e9930
Obadofin Emmanuel <obadofinadedapo123@gmail.com>
2025-08-12 17:04:27 +0100

    cleaned up the js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const ALL_MUSIC = [
     artiste:'hans zimmer',
     length:'1:00'
 },
-
     {
     title:'time',
     artiste:'hans zimmer',
@@ -15,30 +14,21 @@ const ALL_MUSIC = [
 }
 ]
 
-
-
 const renderSongs = ()=>{
-    
     ALL_MUSIC.forEach((music)=>{
         const MUSIC_CARD = document.createElement("div")
         MUSIC_CARD.className="one_music_card"
 
-        MUSIC_CARD.innerHTML=`<div class="music_image">
-
-                        </div>
-                        <div>
-                            <span class="song_name">${music.title}</span>
-                            <span class="artiste_name">${music.artiste}</span>
-                        </div>
-                        
-                        <span class="time" style="margin-left: 300px;">${music.length}</span>`
+        MUSIC_CARD.innerHTML=`
+        <div class="music_image"></div>
+        <div>
+        <span class="song_name">${music.title}</span>
+        <span class="artiste_name">${music.artiste}</span>
+        </div>
+        <span class="time" style="margin-left: 300px;">${music.length}</span>`
         
         musicList.appendChild(MUSIC_CARD);
     })
-
-   
 }
-
-
 
 renderSongs()
